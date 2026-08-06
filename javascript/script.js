e31b46476223b9db4a3e681b1265cf44bfcaf18a
Pushpendra Sharma <pushpendra.sharma_cs.h24@gla.ac.in>
2026-08-06 19:44:14 +0530

    Added new Tasks

---
+++ javascript/script.js
@@ -0,0 +1,20 @@
+let data = [1,2,3,4,5,6,7,8,9,10];
+var i;
+for(i = 0; i < data.length; i++){
+  //console.log(data[i]**2);
+}
+
+var add = (a,b) =>{
+  console.log(a+b);
+}
+
+//add(5,6);
+
+var fun = (arr) =>{
+    var i;
+    for(i = 0; i < arr.length; i++){
+        console.log(arr[i]);
+    }
+}
+
+fun(data);
